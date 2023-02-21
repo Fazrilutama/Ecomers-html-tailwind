c7a95f8a6a345227880777adab7c7355f7564e8f
Fazrilutama <utamafazril8@gmail.com>
2023-02-21 14:12:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      color: {
+        hijau: "#003E29",
+      },
+    },
+  },
+  plugins: [],
+};
